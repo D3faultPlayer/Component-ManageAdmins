264e7ac27c30d78d90d12dd2d44a24a3603a3d85
MicronEngine <zmigpost@gmail.com>
2013-07-15 14:57:55 +0300

    Update admin.php

--- core/init/admin.php
+++ core/init/admin.php
@@ -2,19 +2,4 @@
 
     require_once BASEPATH . 'core/init/db.php';
     $g_adminAuth = new AdminDBAuthModel();
-
-    // Часть для админки
-    if (strpos(strtolower(GetQuery()), 'admin/') === 0 || GetQuery() === 'admin')
-    {
-        // Меняем папку куда будут складироваться css/js админки
-        $g_config['extrapacker']['dir'] = 'extrapacker_admin';
-        $g_config['mainTpl']            = 'admin/main_template';
-
-        if (GetQuery() !== 'admin')
-        {
-            $g_adminAuth->AutoLogin();
-        }
-    }
-
-    define('IS_ADMIN_AUTH', $g_adminAuth->IsAuth());
 ?>
